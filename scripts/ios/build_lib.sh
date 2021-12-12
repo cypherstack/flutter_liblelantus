2984faf1e5c1850d8a6a6c2a0a8c1433bc6e3fd2
Marco Salazar <marcosalazar@Marcos-MacBook-Pro.local>
2021-12-11 17:05:56 -0700

    Fixed IOS plugin build. The problem was a dangling pointer.

--- scripts/ios/build_lib.sh
+++ scripts/ios/build_lib.sh
@@ -4,4 +4,4 @@
 . ./config.sh
 
 cd $MOBILE_LIB_ROOT
-cmake . -DCMAKE_TOOLCHAIN_FILE="${IOS_TOOLCHAIN_ROOT}/toolchain/iOS.cmake" && make
+cmake . -DCMAKE_TOOLCHAIN_FILE="${IOS_TOOLCHAIN_ROOT}/toolchain/iOS.cmake" && make -j4
