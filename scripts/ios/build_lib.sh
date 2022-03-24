a7950d1ba35e29a02636976f20c85edcf9a2ff4c
Marco Salazar <marco@cypherstack.com>
2022-03-24 13:48:41 +0800

    Put Minimum OS version for IOS compilation

--- scripts/ios/build_lib.sh
+++ scripts/ios/build_lib.sh
@@ -5,3 +5,5 @@
 
 cd $MOBILE_LIB_ROOT
 cmake . -DCMAKE_TOOLCHAIN_FILE="${IOS_TOOLCHAIN_ROOT}/toolchain/iOS.cmake" && make -j4
+
+sed -i '' "s/<dict>/<dict>\n\t<key>MinimumOSVersion<\/key>\n\t<string>11.0<\/string>/g" ./mobileliblelantus.framework/Info.plist
