fd58130995fa73e6f78caf6c1c3db302cf856ad8
sneurlax <sneurlax@gmail.com>
2022-10-24 10:32:19 -0500

    clone zlib

--- scripts/windows/build_openssl.ps1
+++ scripts/windows/build_openssl.ps1
@@ -8,3 +8,8 @@ $env:OPENSSL_SHA256="892a0875b9872acd04a9fde79b1f943075d5ea162415de3047c327df33f
 $env:ZLIB_DIR="${env:WORKDIR}\zlib"
 $env:ZLIB_TAG="v1.2.11"
 $env:ZLIB_COMMIT_HASH="cacf7f1d4e3d44d871b605da3b647f07d718623f"
+
+
+if (!(Test-Path -Path $env:ZLIB_DIR)) {
+    git clone -b $env:ZLIB_TAG --depth 1 https://github.com/madler/zlib $env:ZLIB_DIR
+}
