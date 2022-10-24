493558b51b4ce336f6a70f6628ce036fc7f89984
sneurlax <sneurlax@gmail.com>
2022-10-24 10:39:32 -0500

    build zlib

--- scripts/windows/build_all.ps1
+++ scripts/windows/build_all.ps1
@@ -1,4 +1,6 @@
 #! /usr/bin/pwsh
 
+Write-Output "Execute this script from a Visual Studio Developer PowerShell"
+
 New-Item -ItemType Directory -Force -Path build
 ./build_openssl.ps1
--- scripts/windows/build_openssl.ps1
+++ scripts/windows/build_openssl.ps1
@@ -13,3 +13,8 @@ $env:ZLIB_COMMIT_HASH="cacf7f1d4e3d44d871b605da3b647f07d718623f"
 if (!(Test-Path -Path $env:ZLIB_DIR)) {
     git clone -b $env:ZLIB_TAG --depth 1 https://github.com/madler/zlib $env:ZLIB_DIR
 }
+cd $env:ZLIB_DIR
+git reset --hard $env:ZLIB_COMMIT_HASH
+#./configure --static # TODO translate to PowerShell
+cmake # This and the next line require these scripts to be ran from a Visual Studio Developer PowerShell (or cmake and msbuild need to be in PATH)
+msbuild zlib.sln
