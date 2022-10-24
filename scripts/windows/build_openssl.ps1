434e81d53ef4bbd7d86bd4b3dc57372808f00a9e
sneurlax <sneurlax@gmail.com>
2022-10-24 10:25:58 -0500

    set us up the environmental variables

--- scripts/windows/build_openssl.ps1
+++ scripts/windows/build_openssl.ps1
@@ -1,3 +1,10 @@
 #! /usr/bin/pwsh
 
 ./config.ps1
+$env:OPENSSL_FILENAME="openssl-1.1.1k.tar.gz"
+$env:OPENSSL_FILE_PATH="${env:WORKDIR}\${env:OPENSSL_FILENAME}"
+$env:OPENSSL_SRC_DIR="${env:WORKDIR}\openssl-1.1.1k"
+$env:OPENSSL_SHA256="892a0875b9872acd04a9fde79b1f943075d5ea162415de3047c327df33fbaee5"
+$env:ZLIB_DIR="${env:WORKDIR}\zlib"
+$env:ZLIB_TAG="v1.2.11"
+$env:ZLIB_COMMIT_HASH="cacf7f1d4e3d44d871b605da3b647f07d718623f"
