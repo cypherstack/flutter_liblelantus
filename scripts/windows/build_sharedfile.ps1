de355945a9aa2b819bdfa70ea6ba1596eaeb55d4
sneurlax <sneurlax@gmail.com>
2022-10-24 13:34:01 -0500

    update templates

--- scripts/windows/build_sharedfile.ps1
+++ scripts/windows/build_sharedfile.ps1
@@ -1,10 +1,10 @@
 # !/bin/pwsh
 
-#echo ''$(git log -1 --pretty=format:"%H")' '$(date) >> build/git_commit_version.txt
+$env:COMMIT = $(git log -1 --pretty=format:"%H")
+New-Item "build\git_commit_version.txt" -ItemType File -Value $env:COMMIT -Force
 $env:VERSIONS_FILE = "..\..\lib\git_versions.dart"
 $env:EXAMPLE_VERSIONS_FILE = "..\..\lib\git_versions_example.dart"
 Copy-Item $env:EXAMPLE_VERSIONS_FILE -Destination $env:VERSIONS_FILE
-$env:COMMIT = $(git log -1 --pretty=format:"%H")
 $env:OS = "WINDOWS"
 (Get-Content $env:VERSIONS_FILE).replace('WINDOWS_VERSION = ""', "WINDOWS_VERSION = ""${env:COMMIT}""") | Set-Content $env:VERSIONS_FILE
 cd build
--- scripts/windows/linkDistros.ps1
+++ scripts/windows/linkDistros.ps1
@@ -1,10 +1,12 @@
 # !/bin/pwsh
 
-$env:WORKDIR="${pwd}\build"
+$env:WORKDIR="${pwd}/build"
 
-$TeamCityConfPath = 'C:\TeamCity\buildAgent\conf\buildAgent.properties'
+$pattern = '[\\/]'
+$env:CWORKDIR = $env:WORKDIR -replace $pattern, '/'
+
 Copy-Item .\CMakeLists\mobileliblelantus\template_CMakeLists.txt -Destination .\CMakeLists\mobileliblelantus\CMakeLists.txt
-(Get-Content .\CMakeLists\mobileliblelantus\CMakeLists.txt).replace('/opt/android', $env:WORKDIR) | Set-Content .\CMakeLists\mobileliblelantus\CMakeLists.txt
+(Get-Content .\CMakeLists\mobileliblelantus\CMakeLists.txt).replace('/opt/android', $env:CWORKDIR) | Set-Content .\CMakeLists\mobileliblelantus\CMakeLists.txt
 Copy-Item .\CMakeLists\secp256k1\template_CMakeLists.txt -Destination .\CMakeLists\secp256k1\CMakeLists.txt
-(Get-Content .\CMakeLists\secp256k1\CMakeLists.txt).replace('/opt/android', $env:WORKDIR) | Set-Content .\CMakeLists\secp256k1\CMakeLists.txt
+(Get-Content .\CMakeLists\secp256k1\CMakeLists.txt).replace('/opt/android', $env:CWORKDIR) | Set-Content .\CMakeLists\secp256k1\CMakeLists.txt
 Write-Output "edited CMakeLists"
