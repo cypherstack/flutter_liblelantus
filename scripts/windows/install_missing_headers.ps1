ec2c1d14a126dc9443a5a7fbc4c87f5c995ce4b0
sneurlax <sneurlax@gmail.com>
2022-10-25 14:52:26 -0500

    create folder if it doesn't exist

--- scripts/windows/install_missing_headers.ps1
+++ scripts/windows/install_missing_headers.ps1
@@ -2,4 +2,5 @@
 
 Write-Output "installing missing headers"
 
+New-Item -ItemType Directory -Force -Path build/openssl/include
 Copy-Item "CMakeLists\missingheader\endian.h" -Destination "build\openssl\include\"
