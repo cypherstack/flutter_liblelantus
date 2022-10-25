2413b36a182c4269831a2107387c95bf5a7367ab
sneurlax <sneurlax@gmail.com>
2022-10-25 14:53:30 -0500

    add note to continue in MSYS2

--- scripts/windows/build_sharedfile.ps1
+++ scripts/windows/build_sharedfile.ps1
@@ -8,6 +8,7 @@ Copy-Item $env:EXAMPLE_VERSIONS_FILE -Destination $env:VERSIONS_FILE
 $env:OS = "WINDOWS"
 (Get-Content $env:VERSIONS_FILE).replace('WINDOWS_VERSION = ""', "WINDOWS_VERSION = ""${env:COMMIT}""") | Set-Content $env:VERSIONS_FILE
 # Disabled because final build needs to be done by MSYS2
+Write-Output "Continue build process in MSYS2 (Build mobileliblelantus)"
 #cd build
 #cmake mobileliblelantus
 #msbuild mobileliblelantus.sln
