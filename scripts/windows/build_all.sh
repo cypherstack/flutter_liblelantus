89220d289e016c89a9e8c6be232a98f65f582e37
sneurlax <sneurlax@gmail.com>
2022-11-03 12:30:23 -0500

    update submodules and remove cloning snippet

--- scripts/windows/build_all.sh
+++ scripts/windows/build_all.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 
+: '
 # Clone flutter_liblelantus
 mkdir -p ~/src
 cd ~/src
 git clone https://github.com/cypherstack/flutter_liblelantus
 cd flutter_liblelantus
+git submodule update --init --recursive
 #git checkout build/mxe
 #git reset af6c304fd02b145d1d5e21e6341f9a71f756c09f --hard
 
-cd scripts/windows
+cd scripts/windows'
 cp ./CMakeLists/missingheader/windows.h              ./build/include/windows.h
 ./build_openssl.sh
 
