a1543f9ce725053f41064a2935f478d6370b987c
Dan Miller <dan@cypherstack.com>
2022-12-14 10:33:13 -0800

    Fix incorrect variable in openssl build script.

--- scripts/ios/build_openssl.sh
+++ scripts/ios/build_openssl.sh
@@ -8,7 +8,7 @@ OPEN_SSL_DIR_PATH="${EXTERNAL_IOS_SOURCE_DIR}/OpenSSL"
 echo "============================ OpenSSL ============================"
 
 echo "Cloning Open SSL from - $OPEN_SSL_URL"
-git clone "$OPEN_SSL_DIR_PATH" "$OPEN_SSL_DIR_PATH"
+git clone "$OPEN_SSL_URL" "$OPEN_SSL_DIR_PATH"
 cd "$OPEN_SSL_DIR_PATH" || exit 1
 git checkout b77ace70b2594de69c88d0748326d2a1190bbac1
 sed -i '' "s/IOS_MIN_SDK_VERSION=\"12.0\"/IOS_MIN_SDK_VERSION=\"10.0\"/g" build-libssl.sh
