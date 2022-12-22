dae6f4ba4500a656d4d2d225ad463ee3824fa4b5
sneurlax <sneurlax@gmail.com>
2022-12-22 13:27:48 -0600

    remove msvc and msys2 folders and move CMakeLists files back to parent

--- scripts/windows/msvc/install_missing_headers.ps1
+++
@@ -1,6 +0,0 @@
-# !/bin/pwsh
-
-Write-Output "installing missing headers"
-
-New-Item -ItemType Directory -Force -Path build/prefix_x86_64/openssl/include
-Copy-Item "CMakeLists\missingheader\endian.h" -Destination "build\prefix_x86_64\openssl\include\"
--- scripts/windows/msys2/mingw64deps.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-pacman -S --needed --noconfirm base-devel mingw-w64-x86_64-toolchain mingw-w64-x86_64-openssl mingw-w64-x86_64-expat git # openssl-devel
