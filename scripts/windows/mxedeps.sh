892e23472db2f7ee7d998cf2dcc89735d1576eaf
sneurlax <sneurlax@gmail.com>
2022-12-22 13:15:17 -0600

    add g++-mingw-w64-x86-64 dep and expand lines for readability

--- scripts/windows/mxedeps.sh
+++ scripts/windows/mxedeps.sh
@@ -1,7 +1,47 @@
 #!/bin/sh
 
 # MXE dependencies
-sudo apt-get install -y p7zip-full autoconf automake autopoint bash bison bzip2 cmake flex gettext git g++ gperf intltool libffi-dev libtool libtool-bin libltdl-dev libssl-dev libxml-parser-perl make openssl patch perl pkg-config python ruby scons sed unzip wget xz-utils g++-multilib libc6-dev-i386 lzip
+sudo apt-get install -y p7zip-full \
+  autoconf \
+  automake \
+  autopoint \
+  bash \
+  bison \
+  bzip2 \
+  cmake \
+  flex \
+  gettext \
+  git \
+  g++ \
+  gperf \
+  intltool \
+  libffi-dev \
+  libtool \
+  libtool-bin \
+  libltdl-dev \
+  libssl-dev \
+  libxml-parser-perl \
+  make \
+  openssl \
+  patch \
+  perl \
+  pkg-config \
+  python \
+  ruby \
+  scons \
+  sed \
+  unzip \
+  wget \
+  xz-utils \
+  g++-multilib \
+  libc6-dev-i386 \
+  lzip
+
+# MinGW64 dependencies
+# We could make these with MXE like "make cc cmake gcc MXE_TARGETS..." below, but the apt packages are smaller, quicker, and work, so we use them instead of downloading and making them
+sudo apt-get install -y \
+  gcc-mingw-w64-x86-64 \
+  g++-mingw-w64-x86-64
 
 # Install MXE
 mkdir -p ~/development
@@ -13,6 +53,3 @@ if ! [[ $PATH == *"/mxe"* ]]; then
   echo 'export PATH="$HOME/development/mxe/usr/bin:$PATH"' >> ~/.bashrc  # Prepend to PATH
   source ~/.bashrc
 fi
-make cmake MXE_TARGETS='x86_64-w64-mingw32.static'
-
-sudo apt-get install -y gcc-mingw-w64-x86-64
