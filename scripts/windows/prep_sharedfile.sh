97355271fedbf58501695e4cd9af3cf1948f300e
Josh Babb <sneurlax@gmail.com>
2023-06-08 10:53:13 -0500

    add git version snippet to windows build script

--- scripts/windows/prep_sharedfile.sh
+++ scripts/windows/prep_sharedfile.sh
@@ -12,6 +12,16 @@ cp -r ../../mobileliblelantus/* ./build/mobileliblelantus
 sed "s/SET(distribution_DIR \/opt\/android)/SET(distribution_DIR $SEDWORKDIR)/g" ./CMakeLists/mobileliblelantus/template_CMakeLists.txt > ./CMakeLists/mobileliblelantus/CMakeLists.txt
 sed "s/SET(distribution_DIR \/opt\/android)/SET(distribution_DIR $SEDWORKDIR)/g" ./CMakeLists/secp256k1/template_CMakeLists.txt > ./CMakeLists/secp256k1/CMakeLists.txt
 
+echo ''$(git log -1 --pretty=format:"%H")' '$(date) >> build/git_commit_version.txt
+VERSIONS_FILE=../../lib/git_versions.dart
+EXAMPLE_VERSIONS_FILE=../../lib/git_versions_example.dart
+if [ ! -f "$VERSIONS_FILE" ]; then
+    cp $EXAMPLE_VERSIONS_FILE $VERSIONS_FILE
+fi
+COMMIT=$(git log -1 --pretty=format:"%H")
+OS="WINDOWS"
+sed -i "/\/\*${OS}_VERSION/c\\/\*${OS}_VERSION\*\/ const ${OS}_VERSION = \"$COMMIT\";" $VERSIONS_FILE
+
 cp ./CMakeLists/mobileliblelantus/dart_interface.cpp ./build/mobileliblelantus/src/dart_interface.cpp
 cp ./CMakeLists/mobileliblelantus/Utils.cpp          ./build/mobileliblelantus/src/Utils.cpp
 cp ./CMakeLists/mobileliblelantus/Utils.h            ./build/mobileliblelantus/src/Utils.h
