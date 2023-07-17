7d4b1fec59bb06d723f68d5582d6292e5de57682
julian-CStack <julian@cypherstack.com>
2023-07-17 14:33:45 -0600

    update to flutter 3xx and dart 3xx

--- test/flutter_liblelantus_method_channel_test.dart
+++ test/flutter_liblelantus_method_channel_test.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/services.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:flutter_liblelantus/flutter_liblelantus_method_channel.dart';
+import 'package:lelantus/flutter_liblelantus_method_channel.dart';
 
 void main() {
   MethodChannelFlutterLiblelantus platform = MethodChannelFlutterLiblelantus();
--- test/flutter_liblelantus_test.dart
+++ test/flutter_liblelantus_test.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/services.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:flutter_liblelantus/flutter_liblelantus.dart';
+import 'package:lelantus/flutter_liblelantus.dart';
 
 void main() {
   const MethodChannel channel = MethodChannel('flutter_liblelantus');
