7a95bee8237f46fefbadbf6c1374ad6d44bb0a15
Julian <julian@cypherstack.com>
2024-05-08 13:25:13 -0600

    xcode 15 hack

--- scripts/ios/build_lib.sh
+++ scripts/ios/build_lib.sh
@@ -15,4 +15,7 @@ sed -i '' "/\/\*${OS}_VERSION/c\\/\*${OS}_VERSION\*\/ const ${OS}_VERSION = \"$C
 cd $MOBILE_LIB_ROOT
 cmake . -DCMAKE_TOOLCHAIN_FILE="${IOS_TOOLCHAIN_ROOT}/toolchain/iOS.cmake" && make -j4
 
+plutil -replace CFBundleShortVersionString -string "0.0.2" ./mobileliblelantus.framework/Info.plist
+plutil -replace CFBundleVersion -string "1" ./mobileliblelantus.framework/Info.plist
+
 sed -i '' "s/<dict>/<dict>\n\t<key>MinimumOSVersion<\/key>\n\t<string>11.0<\/string>/g" ./mobileliblelantus.framework/Info.plist
