af058ad5a803246395c44c98261a2ae013feebd7
Dan Miller <dan@cypherstack.com>
2024-10-02 10:00:50 -0700

    Exit if errors in windows build.

--- scripts/windows/build_all.sh
+++ scripts/windows/build_all.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-./build_openssl.sh
-./prep_sharedfile.sh
+./build_openssl.sh || exit
+./prep_sharedfile.sh || exit
 
-cd build
+cd build || exit
 rm CMakeCache.txt
 x86_64-w64-mingw32.static-cmake ./mobileliblelantus -DCMAKE_BUILD_TYPE=RelWithDebInfo
 make -j$(nproc)
