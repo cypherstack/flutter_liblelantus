5708e33a0cd413e0525c56ee9cce0367665554e9
julian <julian@cypherstack.com>
2024-10-02 12:51:45 -0600

    and another -L

--- scripts/linux/build_openssl.sh
+++ scripts/linux/build_openssl.sh
@@ -19,7 +19,7 @@ git reset --hard $ZLIB_COMMIT_HASH
 ./configure --static
 make
 
-curl https://www.openssl.org/source/$OPENSSL_FILENAME -o $OPENSSL_FILE_PATH
+curl -L https://www.openssl.org/source/$OPENSSL_FILENAME -o $OPENSSL_FILE_PATH
 echo $OPENSSL_SHA256 $OPENSSL_FILE_PATH | sha256sum -c - || exit 1
 
 for arch in $TYPES_OF_BUILD
